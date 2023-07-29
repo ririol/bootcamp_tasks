0ca96b4fb4961954dfb97665aee7025ebe810c63
Denys <denuss1233@gmail.com>
2023-07-29 16:05:56 +0200

    improved code appearance

--- floodFill.py
+++ floodFill.py
@@ -1,20 +1,21 @@
-def floodFill(image: list[list[int]], sr: int, sc: int, color: int) -> list[list[int]]:
-    start = image[sr][sc]
-    visited = []
-    stack = [[sr,sc]]
-    change = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    
-    while stack:
-        y, x = stack.pop()
-        visited.append([y, x])
-        for dy, dx in change:
-            if not y + dy >= 0 or not y + dy <= len(image)-1: continue
-            if not x + dx >= 0 or not x + dx <= len(image[0])-1: continue
-            if not (image[y + dy][x + dx] == start) or [y + dy,x + dx] in visited: continue           
-            stack.append([y + dy,x + dx])
-    
-    for y,x in visited:
-        image[y][x] = color
-        
-    return image
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        start = image[sr][sc]
+        visited = []
+        stack = [[sr,sc]]
+        change = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
+        while stack:
+            y, x = stack.pop()
+            visited.append([y, x])
+            for dy, dx in change:
+                if not 0 <= y + dy <= len(image)-1: continue        # check y index
+                if not 0 <= x + dx <= len(image[0])-1: continue     # check x index
+                if not (image[y + dy][x + dx] == start): continue   # check color
+                if [y + dy,x + dx] in visited: continue             # check if visited
+                stack.append([y + dy,x + dx]) 
+            
+        for y,x in visited:
+            image[y][x] = color
+            
+        return image
