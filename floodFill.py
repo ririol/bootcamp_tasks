2b11778df3f2ea762b0b83378e36d420c2dcf02c
Denys <denuss1233@gmail.com>
2023-08-04 12:55:06 +0200

    update using set

--- floodFill.py
+++ floodFill.py
@@ -1,18 +1,18 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         start = image[sr][sc]
-        visited = []
+        visited = set()
         stack = [[sr,sc]]
         change = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
+        
         while stack:
             y, x = stack.pop()
-            visited.append([y, x])
+            visited.add((y, x))
             for dy, dx in change:
                 if not 0 <= y + dy <= len(image)-1: continue        # check y index
                 if not 0 <= x + dx <= len(image[0])-1: continue     # check x index
                 if not (image[y + dy][x + dx] == start): continue   # check color
-                if [y + dy,x + dx] in visited: continue             # check if visited
+                if (y + dy,x + dx) in visited: continue             # check if visited
                 stack.append([y + dy,x + dx]) 
             
         for y,x in visited:
